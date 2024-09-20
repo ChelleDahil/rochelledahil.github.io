2d44d0494ca723f062413bf705589f1963893916
ChelleDahil <144247566+ChelleDahil@users.noreply.github.com>
2024-09-21 07:47:48 +0800

    Add files via upload

---
+++ GRP4_DAHIL_EX2.py
@@ -0,0 +1,16 @@
+"""
+Dahil, Rochelle
+BSCS3F
+
+Exercise 2
+
+#3 Creating int,float and complex variable then check the type using the Python Code
+
+"""
+int = 8
+float = 11.12
+complex = 25 + 23j
+
+print("Integer value : ", int)
+print("Float value : ", float)
+print("Complex value : ", complex)
---
+++ grp4_dahil_ex1.py
@@ -0,0 +1,32 @@
+#My first simple script
+# exercise 1
+
+print ("Hello, Im Rochelle Dahil BSCS 3F")
+
+age = 22
+name = "Rochelle"
+gender = "Female"
+
+print("")
+
+print ("Name:",name)
+print ("Age:",age)
+print ("Gender:",gender)
+
+print("")
+rafael = "Rafael"
+mark = "Mark Angehelo"
+hadje = "Hadje"
+cylen = "Cylen"
+
+print (rafael)
+print (mark)
+print (hadje)
+print (cylen)
+
+
+ 
+print("")
+ 
+age,edad,num = 21 ,21, 21
+print(age,edad,num)
